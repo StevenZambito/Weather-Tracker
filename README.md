4efbe8cf79933610ee06ec83fd9f39d2491a428e
Steven Zambito <zambitosteven@gmail.com>
2020-06-10 16:42:35 -0400

    finished project/added README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Current Weather
+# Weather Tracker
+
+<img src="public/assets/weathertracker.gif">
+
+### See it live
+
+http:// .surge.sh/
+
+### Summary
+
+This was the first project I ever used an API with I went through the process of getting an API key with https://openweathermap.org/api and `fetch`ing the weather data from the API for a city or zip code that the user provides. This was also my first time converting a response from an API into JSON format. By using string interpolation, I was able to display the data in a way that's easy to read with the appropriate units.
+
+I am especially proud of using the Moment.js library to convert the time of sunrise/sunset from UNIX to AM/PM as I had never used a library prior to this project.
+
+### Project Goals
+
+- [x] Create an app where the user can look up the current weather
+- [x] Let the user search by city or zip code
+- [x] Add new elements to the DOM upon searching to display the weather data
+
+### Technologies Used
+
+HTML, CSS, JavaScript, Moment.js
--- public/main.js
+++ public/main.js
@@ -1,7 +1,95 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+function getWeatherByCityName() {
+  const cityName = document.getElementById('city').value
+  return axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=5529d8be646bfd216a2b2b7ef4f382c0`
+    )
+    .then((response) => {
+      document.getElementById('weather-in').innerHTML = `The weather in ${
+        response.data.name
+      } is: ${response.data.weather[0].description.toUpperCase()}`
+
+      document.getElementById(
+        'temp'
+      ).innerHTML = `Temperature: ${response.data.main.temp} °F`
+
+      document.getElementById(
+        'max-temp'
+      ).innerHTML = `Max Temp: ${response.data.main.temp_max} °F`
+
+      document.getElementById(
+        'min-temp'
+      ).innerHTML = `Min Temp: ${response.data.main.temp_min} °F`
+
+      document.getElementById(
+        'pressure'
+      ).innerHTML = `Pressure: ${response.data.main.pressure} hPa`
+
+      document.getElementById(
+        'humidity'
+      ).innerHTML = `Humidity: ${response.data.main.humidity}%`
+
+      document.getElementById('sunrise').innerHTML = `Sunrise: ${moment
+        .unix(response.data.sys.sunrise)
+        .format('h:mm a')}`
+
+      document.getElementById('sunset').innerHTML = `Sunset: ${moment
+        .unix(response.data.sys.sunset)
+        .format('h:mm a')}`
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+function getWeatherByZipCode() {
+  const zipCode = document.getElementById('zip').value
+  return axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=5529d8be646bfd216a2b2b7ef4f382c0`
+    )
+    .then((response) => {
+      document.getElementById('weather-in').innerHTML = `The weather in ${
+        response.data.name
+      } is: ${response.data.weather[0].description.toUpperCase()}`
+
+      document.getElementById(
+        'temp'
+      ).innerHTML = `Temperature: ${response.data.main.temp} °F`
+
+      document.getElementById(
+        'max-temp'
+      ).innerHTML = `Max Temp: ${response.data.main.temp_max} °F`
+
+      document.getElementById(
+        'min-temp'
+      ).innerHTML = `Min Temp: ${response.data.main.temp_min} °F`
+
+      document.getElementById(
+        'pressure'
+      ).innerHTML = `Pressure: ${response.data.main.pressure} hPa`
+
+      document.getElementById(
+        'humidity'
+      ).innerHTML = `Humidity: ${response.data.main.humidity}%`
+
+      document.getElementById('sunrise').innerHTML = `Sunrise: ${moment
+        .unix(response.data.sys.sunrise)
+        .format('h:mm a')}`
+
+      document.getElementById('sunset').innerHTML = `Sunset: ${moment
+        .unix(response.data.sys.sunset)
+        .format('h:mm a')}`
+    })
+}
+
+const resetGame = () => {
+  document.getElementById('weather-in').innerHTML =
+    'Welcome to the Weather Tracker!'
+  document.getElementById('temp').innerHTML = 'Temperature:'
+  document.getElementById('max-temp').innerHTML = 'Max Temp:'
+  document.getElementById('min-temp').innerHTML = 'Min Temp:'
+  document.getElementById('pressure').innerHTML = 'Pressure:'
+  document.getElementById('humidity').innerHTML = 'Humidity:'
+  document.getElementById('sunrise').innerHTML = 'Sunrise:'
+  document.getElementById('sunset').innerHTML = 'Sunset:'
+  document.querySelector('#city').value = ''
+  document.querySelector('#zip').value = ''
+}
